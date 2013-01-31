aa2e67f84d10cd03da489d4329363a51be406fc2
Hugo Prudente <hugo.kenshin@gmail.com>
2013-01-31 21:07:25 -0200

    Fully work application imported from filesystem
    Signed-off-by: Hugo Prudente <hugo.kenshin@gmail.com>

---
+++ branch/ExampleService/Process.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ExampleService
+{
+    public static class Process
+    {
+        static Process() { }
+
+        //Start the thread and call the service
+        public static void Start()
+        {
+            Trace.WriteLine("Processo Start()");
+            Thread thread = new Thread(new ThreadStart(doWork));
+            thread.Start();
+            Thread thread2 = new Thread(new ThreadStart(doWorks));
+            thread2.Start();
+        }
+
+        //stop the thread and stop the service
+        public static void Stop()
+        {
+        }
+
+        //action called by the thread and debugmode
+        public static void doWork()
+        {
+            for (int x = 0; x <= 10000; x++)
+            {
+                Trace.WriteLine("x = " + x);
+            }
+        }
+
+        //action called by the thread and debugmode
+        public static void doWorks()
+        {
+            for (int x = 10000; x > 1; x--)
+            {
+                Trace.WriteLine("Y = " + x);
+            }
+        }
+    }
+}
